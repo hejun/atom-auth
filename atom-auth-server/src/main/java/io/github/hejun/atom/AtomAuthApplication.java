425c5332f76ffe8d8664fb279737ec546fe86583
HeJun <1255524587@qq.com>
2025-03-28 11:00:11 +0800

    基础项目结构

---
+++ atom-auth-server/src/main/java/io/github/hejun/atom/AtomAuthApplication.java
@@ -0,0 +1,18 @@
+package io.github.hejun.atom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Atom 认证服务
+ *
+ * @author HeJun
+ */
+@SpringBootApplication
+public class AtomAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtomAuthApplication.class, args);
+	}
+
+}
